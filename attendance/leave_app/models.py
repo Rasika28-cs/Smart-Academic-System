b3b8b67143b2d6f4141d93110670ea000264d611
Rasika28-cs <rasika0828@gmail.com>
2026-06-03 18:50:05 +0530

    added model for grade system

--- attendance/leave_app/models.py
+++ attendance/leave_app/models.py
@@ -315,3 +315,39 @@ class ParentProfile(models.Model):
     student = models.OneToOneField(Student, on_delete=models.CASCADE, related_name='parent')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
+
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class GradeUpload(models.Model):
+    title = models.CharField(max_length=150)
+    semester = models.IntegerField()
+    uploaded_file = models.FileField(upload_to='grades/')
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class StudentGrade(models.Model):
+    upload = models.ForeignKey(
+        GradeUpload,
+        on_delete=models.CASCADE,
+        related_name='grades'
+    )
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    subject_code = models.CharField(max_length=50)
+    subject_name = models.CharField(max_length=150, blank=True, null=True)
+
+    grade = models.CharField(max_length=5)
+
+    class Meta:
+        unique_together = ('upload', 'student', 'subject_code')
+
+    def __str__(self):
+        return f"{self.student} - {self.subject_code}"
