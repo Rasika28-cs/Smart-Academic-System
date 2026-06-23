5aba4fd5e1e1eb747bbcac4be14a6e1d49981404
Aashigasree <aashigasree@gmail.com>
2026-06-23 13:48:41 +0530

    Add whitenoise

--- attendance/myproject/settings.py
+++ attendance/myproject/settings.py
@@ -48,9 +48,9 @@ INSTALLED_APPS = [
 ]
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('do5ma4oaa'),
-    'API_KEY': os.environ.get('783743373927575'),
-    'API_SECRET': os.environ.get('MMVRvpZUflTzCqH-ybrJCEqfizs'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
